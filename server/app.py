77dec22b481ea9373a53a9540bd010c352d14144
Amanda Alcântara <104281621+amandaalbez@users.noreply.github.com>
2025-04-10 20:28:05 -0300

    Remove credenciais sensíveis e ajusta a configuração do banco de dados para uso padrão

--- server/app.py
+++ server/app.py
@@ -31,12 +31,9 @@ socket_logger.addHandler(fh)
 
 # Configuração do Flask e CORS
 app = Flask(__name__)
-from urllib.parse import quote_plus
-
-password = quote_plus('ChatAps@2025')
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://root:{password}@localhost:3306/chatAps'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost:3306/chatAps' 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'ChatAps@2025'
+app.config['SECRET_KEY'] = 'secret!'
 CORS(app, resources={r"/*": {"origins": "*"}})
 
 # Inicializa o SQLAlchemy
